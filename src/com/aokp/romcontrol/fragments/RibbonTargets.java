336e28756710d2987627ad0c5af9cbb5d63500c8
John Hany <johnhany97@gmail.com>
2013-04-30 11:10:50 +0300

    Revert "RC: Ribbons Colorize Action Icons"
    This reverts commit 48189077673187e8c4bbcfb94ee1e13885d30df6.

--- src/com/aokp/romcontrol/fragments/RibbonTargets.java
+++ src/com/aokp/romcontrol/fragments/RibbonTargets.java
@@ -131,8 +131,6 @@ public class RibbonTargets extends AOKPPreferenceFragment implements
     private TextView mRibbonIconSpaceVerticalText;
     private SeekBar mRibbonIconSpaceVertical;
     private Switch mRibbonIconVibrate;
-    private TextView mButtonColorizeText;
-    private Switch mButtonColorize;
 
     private int textColor;
     private int ribbonColor;
@@ -348,15 +346,6 @@ public class RibbonTargets extends AOKPPreferenceFragment implements
             @Override
             public void onCheckedChanged(CompoundButton v, boolean checked) {
                 Settings.System.putBoolean(mContentRes, Settings.System.RIBBON_ICON_VIBRATE[arrayNum], checked);
-            }
-        });
-
-        mButtonColorizeText = ((TextView) ll.findViewById(R.id.enable_button_colorize_id));
-        mButtonColorize = (Switch) ll.findViewById(R.id.enable_button_colorize);
-        mButtonColorize.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton v, boolean checked) {
-                Settings.System.putBoolean(mContentRes, Settings.System.RIBBON_ICON_COLORIZE[arrayNum], checked);
             }
         });
 
@@ -647,10 +636,6 @@ public class RibbonTargets extends AOKPPreferenceFragment implements
 
         mRibbonIconVibrate.setChecked(Settings.System.getBoolean(mContentRes,
                 Settings.System.RIBBON_ICON_VIBRATE[arrayNum], true));
-
-        boolean colorize = Settings.System.getBoolean(mContentRes,
-                Settings.System.RIBBON_ICON_COLORIZE[arrayNum], false);
-        mButtonColorize.setChecked(colorize);
 
     }
 
